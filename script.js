4e0684e10beb4c937b74d3b0539febae480f174e
Syed Raza Ali <razaali26296@gmail.com>
2024-01-28 14:59:23 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const grid = document.querySelector(".grid");
+const scoreDisplay = document.querySelector("#score");
 const blockWidth = 100;
 const blockHeight = 20;
 const boardWidth = 560;
+const boardHeight = 300;
+const ballDiameter = 20;
 const userStart = [230, 10];
+const ballStart = [270, 40];
 let currentPositon = userStart;
+let ballCurrentPosition = ballStart;
+let timerId;
+let xDirection = -2;
+let yDirection = 2;
 class block {
   constructor(xAxis, yAxis) {
     this.bottomLeft = [xAxis, yAxis];
@@ -47,6 +55,10 @@ function drawUser() {
   user.style.left = currentPositon[0] + "px";
   user.style.bottom = currentPositon[1] + "px";
 }
+function drawBall() {
+  ball.style.left = ballCurrentPosition[0] + "px";
+  ball.style.bottom = ballCurrentPosition[1] + "px";
+}
 function moveUser(e) {
   switch (e.key) {
     case "ArrowLeft":
@@ -64,3 +76,56 @@ function moveUser(e) {
   }
 }
 document.addEventListener("keydown", moveUser);
+const ball = document.createElement("div");
+ball.classList.add("ball");
+drawBall();
+grid.appendChild(ball);
+function moveBall() {
+  ballCurrentPosition[0] += xDirection;
+  ballCurrentPosition[1] += yDirection;
+  drawBall();
+  checkForCollisions();
+}
+timerId = setInterval(moveBall, 30);
+
+function checkForCollisions() {
+  for (let i = 0; i < blocks.length; i++) {
+    if (
+      ballCurrentPosition[0] > blocks[i].bottomLeft[0] &&
+      ballCurrentPosition[0] < blocks[i].bottomRight[0] &&
+      ballCurrentPosition[1] + ballDiameter > blocks[i].bottomLeft[1] &&
+      ballCurrentPosition[1] < blocks[i].topLeft[1]
+    ) {
+      const allBlocks = Array.from(document.querySelectorAll(".block"));
+      allBlocks[i].classList.remove("block");
+      blocks.splice(i, 1);
+      changeDirection();
+    }
+  }
+  if (
+    ballCurrentPosition[0] >= boardWidth - ballDiameter ||
+    ballCurrentPosition[1] >= boardHeight - ballDiameter ||
+    ballCurrentPosition[0] <= 0
+  ) {
+    changeDirection();
+  } else if (ballCurrentPosition[1] <= 0) {
+    clearInterval(timerId);
+    scoreDisplay.innerHTML = "You lose";
+    document.removeEventListener("keydown", moveUser);
+  }
+}
+function changeDirection() {
+  if (xDirection === 2 && yDirection === 2) {
+    yDirection = -2;
+    return;
+  } else if (xDirection === 2 && yDirection === -2) {
+    xDirection = -2;
+    return;
+  } else if (xDirection === -2 && yDirection === -2) {
+    yDirection = 2;
+    return;
+  } else if (xDirection === -2 && yDirection === 2) {
+    xDirection = 2;
+    return;
+  }
+}
